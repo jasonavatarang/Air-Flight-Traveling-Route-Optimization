655d2480b686006e398ee11ab749ed9e22ddfa3a
Admin <jason8417@icloud.com>
2023-07-18 14:34:39 -0400

    adding class template

--- src/algorithm/A.h
+++ src/algorithm/A.h
@@ -1,2 +1,9 @@
 #pragma once
 #include <iostream>
+
+class A {
+private:
+
+public:
+	A() {};
+};
--- src/algorithm/Dijkstra.h
+++ src/algorithm/Dijkstra.h
@@ -1,1 +1,9 @@
 #pragma once
+#include <iostream>
+
+class Dijisktra {
+private:
+
+public:
+	Dijisktra() {};
+};
