9f3e113998f7c2d88b1dd826daeeb4db3f1bed12
Admin <jason8417@icloud.com>
2023-07-24 15:46:24 -0400

    testing

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -62,7 +62,32 @@ vector<string> Graph::findShortestPath_BFS(string from, string to)
 
 vector<string> Graph::findShortestPath_Astar(string from, string to)
 {
-	return vector<string>();
+	priority_queue<pair<int, int> > frontier;
+	frontier.push(make_pair(ids[from], 0));
+
+	unordered_map<int, int> came_from;
+	unordered_map<int, int> cost_so_far;
+	came_from[ids[from]] = ids[from];
+	cost_so_far[ids[from]] = 0;
+
+	while (!frontier.empty()) {
+		int current = frontier.top().first;
+
+		if (names[current] == to) {
+			break;
+		}
+		for (pair<int, int> next : adj_list[current]) {
+			//for (Location next : graph.neighbors(current)) {
+			int new_cost = cost_so_far[current] + next.second;
+			if (cost_so_far.find(next.first) == cost_so_far.end()
+				|| new_cost < cost_so_far[next.first]) {
+				cost_so_far[next.first] = new_cost;
+				int priority = new_cost + next.second - adj_list[ids[from]][ids[to]].second; // heuristic(next, goal)
+				frontier.push(make_pair(next.first, priority));
+				came_from[next.first] = current;
+			}
+		}
+	}
 }
 
 vector<string> Graph::findShortestPath_Dijkstra(string from, string to)
