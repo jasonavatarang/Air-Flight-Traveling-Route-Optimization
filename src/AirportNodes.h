7d07d1e39cdd4e04625f9e1d310c7d5caa7390da
Jason Ang <jason8417@icloud.com>
2023-08-03 13:11:51 -0400

    adding sfml main

--- src/AirportNodes.h
+++ src/AirportNodes.h
@@ -3,11 +3,25 @@
 
 class AirportNodes {
 private:
-	float x;
-	float y;
+	float _x;
+	float _y;
 	std::string airport_name;
+    std::vector<AirportNodes*> _neighbors;
 public:
+    AirportNodes();
 	AirportNodes(float x, float y, std::string airport_name);
+    AirportNodes(AirportNodes* node);
+    float x();
+    float y();
+    std::string name();
+    void setX(float x);
+    void setY(float y);
+    void setName(std::string name);
+    void addNeighbor(AirportNodes* node);
+    std::vector<AirportNodes*> neighbors();
+    float distance(AirportNodes* neighbor);
+
+
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,7 @@ int main()
 	d.openRaw("data\\openflights_airports");
 	d.randomizeFlights(500000);
 
-	vector <string> ans;
+	//vector <string> ans;
 	unsigned int cost;
 	unsigned int time;
 
@@ -34,41 +34,52 @@ int main()
 	cout << g.Displacement(from, to) << " km\n\n";
 
 	cout << "Dijkstra:\n";
-	ans = g.Dijkstra(from, to, cost, time);
-	if (ans.size() == 0)
+	vector <string> dijskstra_ans = g.Dijkstra(from, to, cost, time);
+	if (dijskstra_ans.size() == 0)
 		cout << "No path\n";
-	for (string str : ans) {
+	for (string str : dijskstra_ans) {
 		cout << str << " -> ";
 	}
 	cout << "ARRIVED\n";
 	cout << "Distance: " << cost << " km\n";
 	cout << "Used time: " << time << " microsec\n\n";
+
 
 
 	cout << "Astar:\n";
-	ans = g.Astar(from, to, cost, time);
-	if (ans.size() == 0)
+	vector <string> Astar_ans = g.Astar(from, to, cost, time);
+	if (Astar_ans.size() == 0)
 		cout << "No path\n";
-	for (string str : ans) {
+	for (string str : Astar_ans) {
 		cout << str << " -> ";
 	}
 	cout << "ARRIVED\n";
 	cout << "Distance: " << cost << " km\n";
 	cout << "Used time: " << time << " microsec\n\n";
 
-	sf::CircleShape nodeCircles[number_nodes];
 
-	for (int i = 0; i < n_nodes; ++i)
-	{
+	sf::CircleShape* nodeCirclesDijskstra = new sf::CircleShape[dijskstra_ans.size()];
+	sf::CircleShape* nodeCirclesAstar = new sf::CircleShape[Astar_ans.size()];
+
+	std::vector<AirportNodes> nodeListDijskstra(dijskstra_ans.size());
+	std::vector<AirportNodes> nodeListAstar(Astar_ans.size());
+
+	for (int i = 0; i < dijskstra_ans.size(); ++i) {
 		float radius = 10;
-		nodeCircles[i].setRadius(radius);
-		nodeCircles[i].setPosition(sf::Vector2f(nodeList[i]->x() - radius, nodeList[i]->y() - radius));
-		nodeCircles[i].setFillColor(sf::Color(0, 0, 0, 30));
+		nodeCirclesDijskstra[i].setRadius(radius);
+		nodeCirclesDijskstra[i].setPosition(sf::Vector2f(nodeListDijskstra[i].x() - radius, nodeListDijskstra[i].y() - radius));
+		nodeCirclesDijskstra[i].setFillColor(sf::Color(0, 0, 0, 30));
+
+		sf::Text label;
+		label.setString("Airport: " + dijskstra_ans[i]);
+		label.setCharacterSize(12);
+		label.setFillColor(sf::Color::Black);
+		label.setPosition(sf::Vector2f(nodeListDijskstra[i].x() + 2 * radius, nodeListDijskstra[i].y() - 0.5 * radius));
 	}
 
 	sf::ContextSettings settings;
 	settings.antialiasingLevel = 8;
-	sf::RenderWindow window(sf::VideoMode(width, height), "Graph", sf::Style::Default, settings);
+	sf::RenderWindow window(sf::VideoMode(width, height), "Dijskstra Graph", sf::Style::Default, settings);
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -81,16 +92,30 @@ int main()
 		}
 
 		window.clear(sf::Color::White);
-		for (int i = 0; i < n_nodes; ++i)
-		{
-			window.draw(nodeCircles[i]);
-			for (Node* neighbor : nodeList[i]->neighbors())
-			{
-				sf::RectangleShape line = Line(nodeList[i]->x(), nodeList[i]->y(), neighbor->x(), neighbor->y());
-				window.draw(line);
-			}
-		}
+		for (int i = 0; i < dijskstra_ans.size(); ++i) {
+			window.draw(nodeCirclesDijskstra[i]);
+			for (AirportNodes* neighbor : nodeListDijskstra[i].neighbors()) {
+				float x1 = nodeListDijskstra[i].x();
+				float y1 = nodeListDijskstra[i].y();
+				float x2 = neighbor->x();
+				float y2 = neighbor->y();
 
+				// Draw the line
+				sf::RectangleShape line = Line(x1, y1, x2, y2);
+				window.draw(line);
+
+				// Calculate the midpoint between the two nodes
+				float midX = (x1 + x2) / 2.0f;
+				float midY = (y1 + y2) / 2.0f;
+
+				sf::Text label;
+				label.setString("Distance: " + ); // distance
+				label.setCharacterSize(12);
+				label.setFillColor(sf::Color::Black);
+				label.setPosition(sf::Vector2f(midX, midY));
+
+				window.draw(label);
+			}
 		window.display();
 	}
 
