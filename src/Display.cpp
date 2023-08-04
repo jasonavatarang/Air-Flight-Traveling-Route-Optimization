bc07fd5947622e289ed76e56b2029aa5ff30683a
William Dinicola <wdinicola@ufl.edu>
2023-08-04 15:20:34 -0400

    display gui textboxes and button but fon't doesn't load

--- src/Display.cpp
+++ src/Display.cpp
@@ -1,9 +1,30 @@
 #include <thread>
 #include "SFML/Graphics.hpp"
 #include "Graph.h"
+#include <SFML/Window.hpp>
 #include "Display.h"
 
 using namespace sf;
+
+void SetupGUI(RenderWindow& window, Font& font, Text& from_text, Text& to_text, RectangleShape& submit_button)
+{
+	// Set up "from" textbox
+	from_text.setFont(font);
+	from_text.setCharacterSize(20);
+	from_text.setFillColor(sf::Color::Black);
+	from_text.setPosition(50.f, 50.f);
+
+	// Set up "to" textbox
+	to_text.setFont(font);
+	to_text.setCharacterSize(20);
+	to_text.setFillColor(sf::Color::Black);
+	to_text.setPosition(50.f, 100.f);
+
+	// Set up "submit" button
+	submit_button.setSize(Vector2f(100.f, 40.f));
+	submit_button.setFillColor(sf::Color::Green);
+	submit_button.setPosition(50.f, 150.f);
+}
 
 void PromptWindow(Graph& graph)
 {
@@ -11,39 +32,52 @@ void PromptWindow(Graph& graph)
 	unsigned int height = 600;
 	RenderWindow window(VideoMode(width, height), "Prompt");
 
-	//
-	// Add prompts
-	//
-	// Add two input textbox and one button
-	//
+	Font font;
+	if (!font.loadFromFile("font.ttf")) {
+		// Handle font loading failure
+		return;
+	}
+
+	Text from_text, to_text;
+	RectangleShape submit_button;
+
+	SetupGUI(window, font, from_text, to_text, submit_button);
 
 	bool new_path = false;
 	std::string from, to;
-	std::thread GraphWindow_thread([&] {GraphWindow(graph, from, to, new_path); });
-	GraphWindow_thread.detach();
 
 	while (window.isOpen())
 	{
 		Event event;
 		while (window.pollEvent(event))
 		{
-
-			//
-			// Add mouse press event
-			//
-			// When mouse.clicked on button, update from, to, new_path
-			//
-
 			if (event.type == Event::Closed)
 			{
 				window.close();
 			}
+			else if (event.type == Event::MouseButtonPressed)
+			{
+				// Check if the mouse clicked on the submit button
+				Vector2i mousePos = Mouse::getPosition(window);
+				if (submit_button.getGlobalBounds().contains(static_cast<float>(mousePos.x), static_cast<float>(mousePos.y)))
+				{
+					// Update the from, to, and new_path variables
+					from = from_text.getString();
+					to = to_text.getString();
+					new_path = true; // Set the flag to indicate that new input is available
+				}
+			}
 		}
 
 		window.clear(Color::White);
+		// Draw GUI elements
+		window.draw(from_text);
+		window.draw(to_text);
+		window.draw(submit_button);
 		window.display();
 	}
 }
+
 
 
 void GraphWindow(Graph& graph, std::string& from, std::string& to, bool& new_path)
