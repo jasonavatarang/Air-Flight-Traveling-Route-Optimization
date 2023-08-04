7a647dbf8c25db7319263a42b9c504088c40c724
FrostleafW <wu.patrick19@gmail.com>
2023-08-04 02:17:54 -0400

    Rework GUI

--- src/Display.h
+++ src/Display.h
@@ -2,3 +2,7 @@
 
 void PromptWindow(void* graph);
 void GraphWindow(void* graph, std::string& from, std::string& to, bool& new_path);
+
+std::pair<unsigned int, unsigned int> coord2pixel(std::pair<double, double>& coordinates);
+void airportCreate(std::pair<unsigned int, unsigned int>& pixels);
+void pathCreate(std::pair<unsigned int, unsigned int>& from, std::pair<unsigned int, unsigned int>& to);
